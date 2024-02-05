55385f18e34e5d7cb5c71f8e8ca6ed2fabdb18d2
Casimir Lundberg <clundber@c2r3p6.hive.fi>
2024-02-05 17:11:47 +0200

    basic map reading done

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,11 +6,20 @@
 /*   By: clundber <clundber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 11:25:50 by clundber          #+#    #+#             */
-/*   Updated: 2024/02/01 14:19:50 by clundber         ###   ########.fr       */
+/*   Updated: 2024/02/05 17:06:44 by clundber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG.H
-# define SO_LONG.H
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+#include <fcntl.h>
+#include "../libft/includes/libft.h"
+
+typedef struct s_struct
+{
+char **map;
+
+} t_struct;
 
 #endif
--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,18 +6,55 @@
 /*   By: clundber <clundber@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 16:45:45 by clundber          #+#    #+#             */
-/*   Updated: 2024/02/05 15:39:46 by clundber         ###   ########.fr       */
+/*   Updated: 2024/02/05 17:11:14 by clundber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "/includes/so_long.h"
+#include "../includes/so_long.h"
+
+
+
+void	get_map(t_struct *lstruct, char **argv)
+
+{
+	int	i;
+	int	fd;
+	int	k;
+
+	k = 0;
+	i = 0;
+	fd = open(argv[1], O_RDONLY);
+	while (get_next_line(fd))
+		i++;
+	close(fd);
+	lstruct->map = malloc(sizeof(char*) * (i +1));
+	fd = open(argv[1], O_RDONLY);
+	while (i >= 0)
+	{
+		lstruct->map[k] = get_next_line(fd);
+		k++;
+		i--;
+	}
+	lstruct->map[k] = NULL;
+	close(fd);
+	i = 0;
+	while (lstruct->map[i])
+	{
+		ft_printf("%s\n", lstruct->map[i]);
+		i++;
+	}
+}
 
 int	main(int argc, char *argv[])
 
 {
-	
-
-
+	t_struct lstruct;		
+	if (argc != 2)
+	{
+		ft_putstr_fd("Error\n", 2);
+		exit(1);
+	}	
+	get_map(&lstruct, argv);
 }
 
 
